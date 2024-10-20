74ee42365c4f371779d80b186d1909022cccf369
gustavo gregorio <gustavogregorio1986@gmail.com>
2024-10-20 17:07:34 -0300

    desenvolvimento da pagina de home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  imagemUrl = 'https://quikdev.com.br/wp-content/uploads/2022/04/empresa-de-desenvolvimento-de-software.png'
+  imagemAlt = 'desenvolviment0o de software'
 }
